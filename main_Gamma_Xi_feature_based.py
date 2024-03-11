75712161e9d27a378e9a8c647417999457ab052e
Mohamad Louai Shehab <mlshehab@umich.edu>
2024-03-11 17:00:04 -0400

    generate basis for feature

--- main_Gamma_Xi_feature_based.py
+++ main_Gamma_Xi_feature_based.py
@@ -18,19 +18,20 @@ if __name__ == '__main__':
     wind = 0.1
     discount = 0.9
     horizon =   13   
-    start_state = 24  
+    start_state = 24 
     feature_dim = 2
     # p1 = 20 # position of culvers burger joint
     # p2 = 20 # position of charging station
 
     theta = 5*np.ones((2,1))
 
-    landmark_locations = [(0,24), (1,3), (4,20), (14,14)]
-    color = ['blue', 'red','green', 'magenta']
+    landmark_locations = [(0,24), (1,3), (3,19), (14,14)]
+    color = ['blue', 'red','green', 'cyan']
+    style = ["-","--",":","-."]
     overall_min = 1e6
     overall_max = -1e6
     
-    for landmark_loc,col in zip(landmark_locations, color):
+    for landmark_loc,col,st in zip(landmark_locations, color,style):
         # print(start_state)
         t_step = []
         data = []
@@ -52,7 +53,8 @@ if __name__ == '__main__':
             # quick sanity check
             # v = np.linalg.pinv(Gamma) @ Xi    
             # assert np.linalg.norm(Gamma@v - Xi) <= 1e-5
-            A = projected_K
+            A = scipy.linalg.orth(projected_K)
+            # print("A", A.shape)
             B = F
             # print("Shape of B is: ", B.shape)
 
@@ -63,18 +65,24 @@ if __name__ == '__main__':
             V_p = V[:A.shape[1]]
 
             intersection = A@V_p
+            # print(intersection.shape)
             # print(np.linalg.matrix_rank(intersection))
-            data.append( np.linalg.matrix_rank(intersection))
+            # if np.linalg.matrix_rank(intersection) == 100:
+            #     print("A:", A.shape)
+            #     print("B:", B.shape)
+            #     print("V:", V.shape)
+            # data.append( np.linalg.matrix_rank(intersection))
+            data.append(intersection.shape[1])
             t_step.append(t)
             # print(t)
         
-        # # print("min is: ", min(data))    
+        # print("min is: ", min(data))    
         if min(data) < overall_min:
             overall_min = min(data)
         if max(data)> overall_max:
             overall_max = max(data)
         
-        plt.plot(t_step, data, linestyle='--',  color= col, linewidth = 2.0, alpha=0.5, label = f"landmark location = {(p1,p2)}")    
+        plt.plot(t_step, data, linestyle=st,  color= col, linewidth = 1.0, alpha=0.8, label = f"landmark location = {(p1,p2)}")    
         # plt.plot(t_step, n_unreach_states, linestyle=':',  color= col, linewidth = 1.0, alpha=0.5, label = f"start state = {start_state}")
 
     plt.axhline(y = overall_min, color = 'black', linestyle=':',linewidth = 3.0, label = f'min dim = {overall_min}')
